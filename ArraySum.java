cfd554fab5f139f96c40f4e8e08021daf0cdeac4
Samiksha Awasthi <128209850+SamikshaAwasthi@users.noreply.github.com>
2023-10-09 00:18:10 +0530

    second commit

---
+++ ArraySum.java
@@ -0,0 +1,15 @@
+/**
+ * ArraySum
+ */
+public class ArraySum {
+
+    public static void main(String[] args) {
+        int arr[] = {1,290,33,221,44};
+        int sum = 0 ;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum+arr[i];
+        }
+
+        System.out.println(sum);
+    }
+}
